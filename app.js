25c80aeec1e9c599671612d478664f2ef8e05fca
Upasana Mahanta <ups.win@gmail.com>
2017-06-10 01:34:38 +0530

    DI update again3

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 (function(){
   'use restrict';
   angular.module('myApp',[]).controller('myFC', callmyFC);
-  function callmyFC($scope){
+  function callmyFC($scope, $filter){
 
-    $scope.name="Upasana Mahanta";
+  //  $scope.name="Upasana Mahanta";
+
+    $scope.upp=function(){
+      var upcase = $filter('uppercase');
+      $scope.name = upcase($scope.name);
+    };
 
   }
 })();
