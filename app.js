697407ee3125f37739a38175e330e3f73c22a555
Upasana Mahanta <ups.win@gmail.com>
2017-06-10 01:39:53 +0530

    DI update again3

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
   'use restrict';
   angular.module('myApp',[]).controller('myFC', callmyFC);
   function callmyFC($scope, $filter){
-
     $scope.upp=function(){
       var upcase = $filter('uppercase');
       $scope.name = upcase($scope.name);
