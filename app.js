f9fb5f9b9a66dafae3911c9853e0c5680f465095
Upasana Mahanta <ups.win@gmail.com>
2017-06-10 01:35:56 +0530

    DI update again3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
   angular.module('myApp',[]).controller('myFC', callmyFC);
   function callmyFC($scope, $filter){
 
-  //  $scope.name="Upasana Mahanta";
+    $scope.name="Upasana Mahanta";
 
     $scope.upp=function(){
       var upcase = $filter('uppercase');
