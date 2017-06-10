069b8183ca2ab1b0755d0e51b204ebdcd62b890d
Upasana Mahanta <ups.win@gmail.com>
2017-06-10 19:48:09 +0530

    filter commit

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 (function(){
   'use restrict';
-  angular.module('myApp',[]).controller('myFC', callmyFC);
-  function callmyFC($scope, $filter){
+  angular.module('myApp',[]).controller('myFC', callmyFC).filter("myfilter",myFilterFac);
+
+  callmyFC.$inject = ['$scope','$filter','myfilterFilter'];
+
+  function callmyFC($scope, $filter, myfilterFilter){
     $scope.upp=function(){
       var upcase = $filter('uppercase');
       $scope.name = upcase($scope.name);
     };
-
-  }
+    var msg = $scope.name;
+    myfilterFilter(msg);
+  };
 })();
+
+function myFilterFac(){
+  return function(ip){
+      return ip+"***filter";
+  };
+}
